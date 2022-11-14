cc01703672f5bd136075cacaee8e224ba7fbcfb5
Prajwal Kulkarni <kulkarniprajwal.01@gmail.com>
2022-11-14 15:51:09 +0530

    Integrated firestore
    Once the user is authenticated, the user id, user type and email-id is passed to server

--- backend/index.js
+++ backend/index.js
@@ -12,32 +12,16 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGE_SENDER_ID,
-    appId: process.env.APP_ID,
-    measurementId: process.env.MEASUREMENT_ID,
-};
-
-firebase_app_initialized = firebase_app.initializeApp(firebaseConfig);
-const auth = firebase_auth.getAuth(firebase_app_initialized);
-
 app.get("/", (req, res) => {
+    res.redirect("/login");
+});
+
+app.get("/login", (req, res) => {
     res.json({ message: "Hello from server!" });
 });
 
-app.post("/formdata", (req, res) => {
-    firebase_auth
-        .signInWithEmailAndPassword(auth, req.body.email, req.body.password)
-        .then((userCredential) => {
-            console.log(userCredential.user.uid);
-        })
-        .catch((err) => {
-            console.log(err.code);
-        });
+app.post("/login", (req, res) => {
+    console.log(`req.body = ${req.body.userType}`);
     return res.json({ message: "hello" });
 });
 
--- frontend/src/components/SignIn.js
+++ frontend/src/components/SignIn.js
@@ -1,73 +1,63 @@
+import { signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+import { doc, getDoc } from "firebase/firestore";
+import { auth, firestore } from "../firebase-config";
 import "bootstrap/dist/css/bootstrap.css";
-import React, { useState, useEffect, useRef } from "react";
+import React, { useRef, useState } from "react";
 
 function SignIn() {
-    const [message, setMessage] = useState("");
-
     const refEmail = useRef(null);
     const refPassword = useRef(null);
 
+    const validateEmail = (email) => /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+
+    const validatePassword = (password) =>
+        /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/.test(password);
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(refEmail.current.value);
-        fetch("http://localhost:4000/formdata", {
-            // Adding method type
-            method: "POST",
 
-            // Adding body or contents to send
-            body: JSON.stringify({
-                email: refEmail.current.value,
-                password: refPassword.current.value,
-            }),
+        if (validatePassword(refPassword.current.value) && validateEmail(refEmail.current.value)) {
+            signInWithEmailAndPassword(auth, refEmail.current.value, refPassword.current.value)
+                .then((user) => {
+                    const docRef = doc(firestore, "users", user._tokenResponse.localId);
+                    getDoc(docRef)
+                        .then((docSnap) => {
+                            if (docSnap.exists()) {
+                                fetch("http://localhost:4000/login", {
+                                    // Adding method type
+                                    method: "POST",
 
-            // Adding headers to the request
-            headers: {
-                "Content-type": "application/json; charset=UTF-8",
-            },
-        })
-            .then((res) => res.json())
-            .then((data) => console.log(data))
-            .catch((err) => console.error(err));
+                                    // Adding body or contents to send
+                                    body: JSON.stringify({
+                                        email: refEmail.current.value,
+                                        userId: user.user.uid,
+                                        userType: docSnap.data().userType,
+                                    }),
+
+                                    // Adding headers to the request
+                                    headers: {
+                                        "Content-type": "application/json; charset=UTF-8",
+                                    },
+                                })
+                                    .then((res) => res.json())
+                                    .then((data) => console.log(data))
+                                    .catch((err) => console.error(err));
+                            }
+                        })
+                        .catch((err) => console.error(err));
+                })
+                .catch((err) => console.error(err));
+        } else {
+        }
     };
 
-    useEffect(() => {
-        fetch("http://localhost:4000")
-            .then((res) => res.json())
-            .then((data) => setMessage(data.message))
-            .catch((err) => console.log(err));
-    }, []);
-
     return (
-        // <div className="d-flex justify-content-center vh-100 align-items-center">
-        //     <Form style={{ maxWidth: "400px" }} className="border border-primary p-4 rounded" onSubmit={handleSubmit}>
-        //         <h6>{message}</h6>
-        //         <Form.Group className="mb-3" controlId="formBasicEmail">
-        //             <Form.Label>Email address</Form.Label>
-        //             <Form.Control type="email" placeholder="Enter email" ref={refContainer}/>
-        //             <Form.Text className="text-muted">
-        //                 We'll never share your email with anyone else.
-        //             </Form.Text>
-        //         </Form.Group>
-
-        //         <Form.Group className="mb-3" controlId="formBasicPassword">
-        //             <Form.Label>Password</Form.Label>
-        //             <Form.Control type="password" placeholder="Password" />
-        //         </Form.Group>
-        //         <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        //             <Form.Check type="checkbox" label="Check me out" />
-        //         </Form.Group>
-        //         <Button variant="primary" type="submit">
-        //             Submit
-        //         </Button>
-        //     </Form>
-        // </div>
         <div className="d-flex justify-content-center vh-100 align-items-center">
             <form
                 onSubmit={handleSubmit}
                 className="border border-primary p-4 rounded"
                 style={{ maxWidth: "400px" }}
             >
-                <h6>{message}</h6>
                 <div class="form-group">
                     <label for="exampleInputEmail1">Email address</label>
                     <input
