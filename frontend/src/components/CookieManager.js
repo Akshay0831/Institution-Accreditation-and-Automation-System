8b500ff45ed114a3fb86e638cabb71fcadb6859d
Prajwal Kulkarni <kulkarniprajwal.01@gmail.com>
2022-11-17 13:14:19 +0530

    Updated package.json

--- frontend/src/components/CookieManager.js
+++
@@ -1,9 +0,0 @@
-import Cookies from "universal-cookie";
-
-const cookies = new Cookies();
-
-const cookieSet = cookies?.set;
-const cookieGet = cookies?.get;
-const cookieRemove = cookies?.remove;
-
-export { cookieSet, cookieGet, cookieRemove };
