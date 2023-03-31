04862ead989df857330be89b0e6aaf25558682b1
Prajwal Kulkarni <kulkarniprajwal.01@gmail.com>
2023-03-31 20:46:01 +0530

    Implementing analytics

--- frontend/src/components/Analytics.jsx
+++ frontend/src/components/Analytics.jsx
@@ -1,15 +1,100 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.css";
-import "../../public/css/styles.css"
+// import React from "react";
+// import { Line } from 'react-chartjs-2';
+// import { Chart as ChartJS, LineElement, PointElement, LinearScale, Title } from 'chart.js';
+// import "bootstrap/dist/css/bootstrap.css";
+// import "../../public/css/styles.css"
 
-function Analytics() {
+// function Analytics() {
+//     ChartJS.register(LineElement, PointElement, LinearScale, Title);
+//     const chartData = {
+//         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+//         datasets: [{
+//             label: '# of Votes',
+//             data: [12, 19, 3, 5, 2, 3],
+//             borderWidth: 1
+//         }]
+//     }
+
+//     return (
+//         <main style={{ color: "margin-top: 58px" }}>
+//             <div className="container pt-4">
+//                 <Line data={chartData} />
+//             </div>
+//         </main>
+//     )
+// }
+
+// export default Analytics;
+
+import React from 'react';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+// import faker from 'faker';
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
+const options = {
+    responsive: true,
+    plugins: {
+        legend: {
+            position: 'bottom',
+        },
+        title: {
+            display: true,
+            text: 'Chart.js Line Chart',
+        },
+    },
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const data = {
+    labels,
+    datasets: [
+        {
+            label: 'Dataset 1',
+            data: labels.map(() => Math.floor(Math.random() * (1000 - (-1000) + 1)) + (-1000)),
+            borderColor: 'rgb(255, 99, 132)',
+            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+        },
+        {
+            label: 'Dataset 2',
+            data: labels.map(() => Math.floor(Math.random() * (1000 - (-1000) + 1)) + (-1000)),
+            borderColor: 'rgb(53, 162, 235)',
+            backgroundColor: 'rgba(53, 162, 235, 0.5)',
+        },
+        {
+            label: 'Target',
+            data: labels.map(() => 200),
+            borderColor: 'rgb(0, 0, 0)',
+            backgroundColor: 'rgb(0, 0, 0)',
+        }
+    ],
+};
+
+export default function Analytics() {
     return (
-        <main style={{ color: "margin-top: 58px" }}>
-            <div className="container pt-4">
-                <h1>Visualization of marks</h1>
+        <main>
+            <div className="container pt-4" style={{ backgroundColor: 'rgba(255, 255, 255, 1)' }}>
+                <Line options={options} data={data} />
             </div>
         </main>
-    )
+    );
 }
-
-export default Analytics;
