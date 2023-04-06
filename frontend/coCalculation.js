d05e6420a13e905cb8b95f28c931bffacc7fd531
Shreesha S <SHREESHA.082@GMAIL.COM>
2023-04-07 00:36:30 +0530

    Adds student data and CO calculated tables to excel sheet

--- frontend/coCalculation.js
+++ frontend/coCalculation.js
@@ -26,6 +26,7 @@ let xPercentageOfMaxMarks = {
         "CO4": 2,
         "CO5": 4
     },
+    "SEE50" : 30,
     "SEE": 36
 }
 
@@ -175,7 +176,7 @@ let jsonData = [
                 "CO4": 5
             },
             "A2": {
-                "CO2": 2,
+                "CO2": "NA",
                 "CO3": 6,
                 "CO4": 1
             },
@@ -489,3 +490,123 @@ for (const obj of copoMappingTable) {
 }
 
 console.log(poAverages)
+
+// const rowsToAdd = jsonData.map(obj => Object.values(obj));
+function flattenObject(obj) {
+    let flattened = {};
+    for (let key in obj) {
+        if (typeof obj[key] === 'object' && obj[key] !== null) {
+        let nestedObj = flattenObject(obj[key]);
+        for (let nestedKey in nestedObj) {
+            flattened[key + '.' + nestedKey] = nestedObj[nestedKey];
+        }
+        } else {
+        flattened[key] = obj[key];
+        }
+    }
+    return flattened;
+}
+
+let studentsForExcel = []
+let i = 0
+const usnArray = jsonData.map(obj => obj.fk_USN);
+
+jsonData.forEach((obj) => {
+    let flattened = flattenObject(obj);
+    const valuesArray = Object.values(flattened);
+    const filteredArr = valuesArray.filter((value) => {
+        return (value === "NA" || value === undefined || Number.isInteger(value));
+    });
+    filteredArr.unshift(usnArray[i])
+    i += 1
+    filteredArr.unshift("Name")
+    filteredArr.unshift(i)
+    let index = filteredArr.length - 1; // get the index of the last but one element
+    let forFifty = parseInt(((filteredArr[index])/60) * 50)
+    filteredArr.splice(index, 0, forFifty);
+    studentsForExcel.push(filteredArr)
+});
+
+
+// const arr = objectToArray(jsonData);
+// console.log(arr);
+
+const XLSX = require('xlsx');
+
+const workbook = XLSX.readFile('test.xlsx');
+const worksheet = workbook.Sheets['Sheet1'];
+const startRow = 116;
+const newRows = XLSX.utils.sheet_add_aoa(worksheet, studentsForExcel, { origin: { r: startRow, c: 0 } });
+// XLSX.writeFile(workbook, 'test.xlsx');
+
+calculatedRowNumber = startRow + i + 1
+calculatedRows = []
+
+let flattenedMinMarks = flattenObject(xPercentageOfMaxMarks);
+const valuesArray = Object.values(flattenedMinMarks);
+valuesArray.unshift("60% of Maximum marks (X)")
+calculatedRows.push(valuesArray)
+
+let index = valuesArray.length - 2;
+let flattenedAboveX = flattenObject(studentsAboveX);
+const valuesArray1 = Object.values(flattenedAboveX);
+valuesArray1.unshift("No. of Students Above X")
+// valuesArray1.splice(index, 0, flattenedAboveX[index]);
+// valuesArray1.push(valuesArray1[valuesArray1 - 1]);
+// valuesArray1.push(valuesArray1.slice(-1)[0])
+valuesArray1.push(valuesArray1[index])
+calculatedRows.push(valuesArray1)
+
+
+let flattenedTotalStudents = flattenObject(totalStudents);
+const valuesArray2 = Object.values(flattenedTotalStudents);
+valuesArray2.unshift("Total Students")
+// console.log(valuesArray2.length)
+// valuesArray2.splice(index, 0, flattenedTotalStudents[index]);
+// valuesArray2.push(valuesArray2[valuesArray2 - 1]);
+// console.log(valuesArray2[index+1])
+valuesArray2.push(valuesArray2[index])
+calculatedRows.push(valuesArray2)
+
+let flattenedCOPercentage = flattenObject(coPercentage);
+const valuesArray3 = Object.values(flattenedCOPercentage);
+// valuesArray3.splice(index, 0, flattenedCOPercentage[index]);
+valuesArray3.unshift("CO Percentage")
+valuesArray3.push(valuesArray3[index])
+// valuesArray3.push(valuesArray3[valuesArray3 - 1]);
+calculatedRows.push(valuesArray3)
+
+let headingsRow = [' ', "C01", "CO2","C01", "CO2","C02", "CO3","C04", "CO2","C03", "CO4","C04", "CO5","C04", "CO5", "SEE", "SEE"]
+calculatedRows.push(headingsRow)
+
+const newRows1 = XLSX.utils.sheet_add_aoa(worksheet, calculatedRows, { origin: { r: calculatedRowNumber, c: 0 } });
+XLSX.writeFile(workbook, 'test.xlsx');
+
+
+
+
+// const mergeRange = {
+//     s: { r: 22, c: 1 },
+//     e: { r: 22, c: 4 }
+//     };
+
+//   // Merge the cells in the specified range (for the Name column only)
+//     worksheet.mergeCells(XLSX.utils.encode_range(mergeRange));
+
+// Define the rows to add as an array of objects
+// const rowsToAdd1 = xPercentageOfMaxMarks.map(obj => Object.values(obj));
+// const rowsToAdd1 = [xPercentageOfMaxMarks].map(obj => [...Object.values(obj), 'merged cells']);
+
+
+// Define the headers for the normal cells
+// const headers = ['Name', 'Age', 'Email'];
+
+// Add the rows to the worksheet starting from line 15
+// const startRow1 = 22;
+// const startCol = 0;
+// let rowNum = startRow1;
+// let newRows1 = XLSX.utils.sheet_add_aoa(worksheet, rowsToAdd1, { origin: { r: rowNum, c: startCol } });
+// rowNum += newRows1.length;
+
+// Save the workbook
+// XLSX.writeFile(workbook, 'test.xlsx');
