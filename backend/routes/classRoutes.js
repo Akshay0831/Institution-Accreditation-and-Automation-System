d3d80be2b06552dad773cc48078cb5c3c056b992
Akshay <akshayrgowda2001@gmail.com>
2023-04-10 17:19:28 +0530

    Basic Class CRUD APIs

--- backend/routes/classRoutes.js
+++ backend/routes/classRoutes.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const mongo = require("../db/mongodb");
-const models = require("../models");
 const router = express.Router();
-const { ObjectId } = require("mongodb");
 
 router.route("/")
     .get(async (req, res) => {
@@ -12,5 +10,26 @@ router.route("/")
         let gotClasses = classes.length > 0;
         res.status(gotClasses ? 200 : 400).json(gotClasses ? classes : "Couldn't fetch classes");
     })
+
+    .post(async (req, res) => {
+        const classObj = req.body.Class;
+        const classAdded = await mongo.addDoc("Class", classObj);
+
+        res.status(classAdded ? 200 : 400).json(classAdded ? "Created new class" : "Couldn't create new class");
+    })
+
+    .put(async (req, res) => {
+
+        const classObj = req.body.Class;
+        const classUpdated = await mongo.updateDoc("Class", { _id: req.body._id }, classObj);
+
+        res.status(classUpdated ? 200 : 400).json(classUpdated ? "Updated class" : "Couldn't update class");
+    })
+
+    .delete(async (req, res) => {
+        const classDeleted = await mongo.deleteDoc("Class", { _id: req.body._id });
+
+        res.status(classDeleted ? 200 : 400).json(classDeleted ? "Class deleted" : "Couldn't delete class");
+    });
 
 module.exports = router;
--- frontend/src/components/admin/UpdateClass.jsx
+++ frontend/src/components/admin/UpdateClass.jsx
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { Card, Form, Button } from "react-bootstrap";
 
 export default function UpdateClass() {
 
+    const navigate = useNavigate();
     const { id } = useParams();
     const isUpdate = Boolean(id);
-    document.title = (isUpdate?"Update":"Add") + " Class";
+    document.title = (isUpdate ? "Update" : "Add") + " Class";
 
     const [departments, setDepartments] = useState([]);
     const [departmentID, setDepartmentID] = useState("");
@@ -16,8 +17,8 @@ export default function UpdateClass() {
     useEffect(() => {
         const fetchData = async () => {
             // console.log((await (await fetch("http://localhost:4000/documents/Department")).json()).filter(doc => doc._id == "64256326539b7e514a91fe64" )[0]);
-            if (isUpdate){
-                const classData =  (await (await fetch("http://localhost:4000/documents/Class")).json()).filter(doc => doc._id==id)[0];
+            if (isUpdate) {
+                const classData = (await (await fetch("http://localhost:4000/documents/Class")).json()).filter(doc => doc._id == id)[0];
                 console.log(classData);
                 setDepartmentID(classData["Department"]);
                 setSemester(classData["Semester"]);
@@ -32,11 +33,14 @@ export default function UpdateClass() {
 
     const onSubmitClicked = (event) => {
         event.preventDefault();
-        let classData = {"Class":{
-            "Department": departmentID,
-            "Semester": semester,
-            "Section": section,
-        }};
+        let classData = {
+            "Class": {
+                "Department": departmentID,
+                "Semester": semester,
+                "Section": section,
+            }
+        };
+        if (isUpdate) classData._id = id;
         fetch(`http://localhost:4000/Class`, {
             // Adding method type
             method: (isUpdate ? "PUT" : "POST"),
@@ -44,6 +48,13 @@ export default function UpdateClass() {
             body: JSON.stringify(classData),
             // Adding headers to the request
             headers: { "Content-type": "application/json; charset=UTF-8" },
+        }).then(res => {
+            if (res.status == 200)
+                navigate("/admin/collectionlist",
+                    {
+                        state: "Class",
+                    });
+            console.log(res.status, res.statusText);
         });
     }
 
@@ -53,7 +64,7 @@ export default function UpdateClass() {
         <main className="pt-5">
             <div className="container">
                 <Card>
-                    <Card.Header className="fs-3">{isUpdate? "Update": "Add"} Class</Card.Header>
+                    <Card.Header className="fs-3">{isUpdate ? "Update" : "Add"} Class</Card.Header>
                     <Card.Body>
                         <Form onSubmit={onSubmitClicked}>
                             <Form.Group className="mb-3">
@@ -67,11 +78,11 @@ export default function UpdateClass() {
                             </Form.Group>
                             <Form.Group className="mb-3">
                                 <Form.Label>Semester: </Form.Label>
-                                <Form.Control type="number" name="semester" id="semester" value={semester} placeholder={"Semester"} onChange={(event) => { setSemester(event.target.value) }} required/>
+                                <Form.Control type="number" name="semester" id="semester" value={semester} placeholder={"Semester"} onChange={(event) => { setSemester(event.target.value) }} required />
                             </Form.Group>
                             <Form.Group className="mb-3">
                                 <Form.Label>Section: </Form.Label>
-                                <Form.Control type="text" name="section" id="section" value={section} placeholder={"Section"} onChange={(event) => { setSection(event.target.value) }} required/>
+                                <Form.Control type="text" name="section" id="section" value={section} placeholder={"Section"} onChange={(event) => { setSection(event.target.value) }} required />
                             </Form.Group>
                             <Button variant="success" type="submit">Submit</Button>
                         </Form>
